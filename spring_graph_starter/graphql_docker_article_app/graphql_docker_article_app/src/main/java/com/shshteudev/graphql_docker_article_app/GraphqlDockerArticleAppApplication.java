748779514fa6e1ca9b1b04b0015354eb101cba8e
Md. Shahariar Hossen <shahariar.hossen@bjitgroup.com>
2022-09-26 12:15:45 +0600

    Adding configuration files

---
+++ spring_graph_starter/graphql_docker_article_app/graphql_docker_article_app/src/main/java/com/shshteudev/graphql_docker_article_app/GraphqlDockerArticleAppApplication.java
@@ -0,0 +1,13 @@
+package com.shshteudev.graphql_docker_article_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlDockerArticleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlDockerArticleAppApplication.class, args);
+	}
+
+}
---
+++ spring_graph_starter/graphql_docker_article_app/graphql_docker_article_app/src/test/java/com/shshteudev/graphql_docker_article_app/GraphqlDockerArticleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shshteudev.graphql_docker_article_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDockerArticleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
